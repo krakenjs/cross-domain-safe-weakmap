ce62cd759bac360db3d16fa64ed2e148b03498cc
Daniel Brain <dbrain@paypal.com>
2017-04-02 01:11:35 -0700

    Do not cache windows

--- src/util.js
+++ src/util.js
@@ -1,16 +1,9 @@
 /* @flow */
 
-let windows = [];
-
 export function isWindow(obj : Object) {
-
-    if (windows.indexOf(obj) !== -1) {
-        return true;
-    }
 
     try {
         if (obj && obj.self === obj) {
-            windows.push(obj);
             return true;
         }
     } catch (err) {

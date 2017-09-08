cdb4bfcc8235b6dafd3c2efa1af29dc0cf21703e
Daniel Brain <dbrain@paypal.com>
2017-09-08 00:57:53 -0700

    Add util.js

---
+++ src/util.js
@@ -0,0 +1,20 @@
+/* @flow */
+
+export function safeIndexOf<T>(collection : Array<T>, item : T) {
+    for (let i = 0; i < collection.length; i++) {
+
+        try {
+            if (collection[i] === item) {
+                return i;
+            }
+        } catch (err) {
+            // pass
+        }
+    }
+
+    return -1;
+}
+
+export function noop(...args) {
+    // pass
+}

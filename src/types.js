ab963a37ae2162723353c12370870122ef55ddb0
Daniel Brain <dbrain@paypal.com>
2017-10-14 14:01:16 -0700

    Fixes for downstream types

---
+++ src/types.js
@@ -0,0 +1,30 @@
+/* @flow */
+
+export type CrossDomainWindowType = {|
+    location : string | Object,
+    self : CrossDomainWindowType,
+    closed : boolean,
+    open : (string, string, string) => CrossDomainWindowType,
+    close : () => void,
+    focus : () => void,
+    frames : Array<CrossDomainWindowType>,
+    opener ? : CrossDomainWindowType,
+    parent : CrossDomainWindowType,
+    length : number,
+    postMessage : (string, string) => void
+|};
+
+export type SameDomainWindowType = Object & {
+    location : string | Object,
+    self : CrossDomainWindowType,
+    closed : boolean,
+    open : (string, string, string) => CrossDomainWindowType,
+    close : () => void,
+    focus : () => void,
+    XMLHttpRequest : typeof XMLHttpRequest,
+    document : Document,
+    navigator : {
+        userAgent : string,
+        mockUserAgent? : string
+    }
+};
